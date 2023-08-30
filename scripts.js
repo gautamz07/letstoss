ec279e0059e57c6bad604783054856cd9731defc
gautamgoshopmatic <gautam.fonseca@goshopmatic.com>
2023-08-30 20:07:25 +0530

    service worker added

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
 document.getElementById('tossIt').addEventListener('click', () => {
   document.getElementById('coin-wrapper').classList.add('tossed')
 });
+
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
+}
